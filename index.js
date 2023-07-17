11ffa61eb5c6f5b2701585ff00111feb08151ee6
Paras Grover <parasgrover932@gmail.com>
2023-07-17 14:40:46 +0530

    Optional Url rendering

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const http = require("http");
 const fs = require("fs");
-const index = fs.readFileSync("./Data.json", "utf-8");
+const index = fs.readFileSync("./index.html", "utf-8");
+const data = fs.readFileSync("./Data.json", "utf-8");
+const error = fs.readFileSync("./error.html", "utf-8");
 const server = http.createServer((req, res) => {
+  switch (req.url) {
+    case "/":
+      res.setHeader("Content-Type", "text/html");
+      res.end(index);
+      break;
+    case "/json":
+      res.setHeader("Content-Type", "application/json");
+      res.end(data);
+      break;
+    default:
+      res.setHeader("Content-Type", "text/html");
+      res.end(error);
+      break;
+  }
   console.log("server started");
-  console.log(req.url);
-  res.end(index);
-  //   res.setHeader("   Content-Type", "application/json");
 });
 
-server.listen(8080);
+server.listen(8000);
