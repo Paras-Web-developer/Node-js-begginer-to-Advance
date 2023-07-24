790bc6a5ea15d5097b2366aba417da70ed3ef2ff
Paras Grover <parasgrover932@gmail.com>
2023-07-24 13:55:09 +0530

    Middleware's application,routes (Auth),Built-in

--- index.js
+++ index.js
@@ -5,21 +5,63 @@ const products = data.products;
 const express = require("express");
 const server = express();
 
+// Middle Ware (Application Level)
+server.use((req, res, next) => {
+  console.log(
+    req.method,
+    req.ip,
+    req.hostname,
+    new Date(),
+    req.get("User-Agent")
+  );
+  next();
+});
 
-// API - Endpoint -Route
-server.get("/", (req, res) => {
+// Middle Ware Particular Path (Router) level middle ware
+
+const Auth = (req, res, next) => {
+  console.log(req.query);
+  if (req.body.password == 123) {
+    next();
+  } else {
+    res.sendStatus(400);
+  }
+};
+// server.use(Auth); // This middleware Auth add all routes
+
+// Built-in =======> middleware
+// 1. express.static
+// 2. express.json
+// 3. express.unlencoded
+
+// 1. express.static =======> Middleware
+server.use(express.static("public"));   
+
+// 2. express.json =======> Middleware
+server.use(express.json());
+
+// 3. express.unlencoded  =======> Middleware
+server.use(express.urlencoded());
+
+// API - Endpoint -Route 
+server.get("/", Auth, (req, res) => {
+  // Auth(middleware) add only this route
+  console.log(req.method, req.ip, req.hostname);
   res.status(200).send({ Type: "GET" });
 });
 
-server.post("/post", (req, res) => {
+server.post("/post", Auth, (req, res) => {
+  console.log(req.method, req.ip, req.hostname);
   res.send({ Type: "POST" });
 });
 
 server.put("/put", (req, res) => {
+  console.log(req.method, req.ip, req.hostname);
   res.json({ Type: "PUT" });
 });
 
 server.delete("/delete", (req, res) => {
+  console.log(req.method, req.ip, req.hostname);
   res.json({ Type: "DELETE" });
 });
 
@@ -27,6 +69,6 @@ server.patch("/patch", (req, res) => {
   res.json({ Type: "PATCH" });
 });
 
-server.listen(8080, () => {
+server.listen(3000, () => {
   console.log("server started");
 });
