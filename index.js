195c86a35a704be4fdb9862cc5b3f8dfb777cb3e
Paras Grover <parasgrover932@gmail.com>
2023-07-24 14:26:37 +0530

    Morgon & params

--- index.js
+++ index.js
@@ -3,24 +3,31 @@ const index = fs.readFileSync("./index.html", "utf-8");
 const data = JSON.parse(fs.readFileSync("./Data.json", "utf-8"));
 const products = data.products;
 const express = require("express");
+const morgan = require("morgan"); // npm i  morgan
 const server = express();
 
 // Middle Ware (Application Level)
-server.use((req, res, next) => {
-  console.log(
-    req.method,
-    req.ip,
-    req.hostname,
-    new Date(),
-    req.get("User-Agent")
-  );
-  next();
-});
+// server.use((req, res, next) => {
+//   console.log(
+//     req.method,
+//     req.ip,
+//     req.hostname,
+//     new Date(),
+//     req.get("User-Agent")
+//   );
+//   next();
+// });
+
+// ^^
+// ||
+// ||
+// morgan provide all these information which i get using req
+server.use(morgan("default"));  //<<<<============================morgan
 
 // Middle Ware Particular Path (Router) level middle ware
 
 const Auth = (req, res, next) => {
-  console.log(req.query);
+  console.log(req.query); // http://localhost:3000/?password=123
   if (req.body.password == 123) {
     next();
   } else {
@@ -35,7 +42,7 @@ const Auth = (req, res, next) => {
 // 3. express.unlencoded
 
 // 1. express.static =======> Middleware
-server.use(express.static("public"));   
+server.use(express.static("public"));            
 
 // 2. express.json =======> Middleware
 server.use(express.json());
@@ -43,11 +50,14 @@ server.use(express.json());
 // 3. express.unlencoded  =======> Middleware
 server.use(express.urlencoded());
 
-// API - Endpoint -Route 
-server.get("/", Auth, (req, res) => {
-  // Auth(middleware) add only this route
-  console.log(req.method, req.ip, req.hostname);
-  res.status(200).send({ Type: "GET" });
+// API - Endpoint -Route  using params
+server.get("/product/:id", (req, res) => {     //<<<============="/product/:id"  Params
+  if (req.params.id == 5) {
+    res.status(200).send({ Type: "GET" });
+  }
+  else{
+    res.status(404).send({Type: "ERROR"})
+  }
 });
 
 server.post("/post", Auth, (req, res) => {
