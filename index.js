6041153aac0c2650f2c1cb5a696c4d539c49ca7b
Paras Grover <parasgrover932@gmail.com>
2023-07-30 22:05:37 +0530

    Add_midle_routes. EX-http://localhost:3000/api/get

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const server = express();
+const productController = require("./controller/product");
+const productRouter = express.Router();  //======> Express routes
 server.use(express.json()); //express.json =======> Middleware
 server.use(express.static("public"));
-const productRouter = express.Router();
-const productController = require("./controller/product");
-// >>>>>>>>>>> C R U D >>>>>>>>>>>> CREATE READ UPDATE DELETE
+server.use('/api',productRouter) //======> Add_Middle_Rou
 
 // MVC model-view-controller
-server
+productRouter
   .post("/post", productController.createProduct)
   .get("/get", productController.getAllData)
   .get("/get/:id", productController.getPerticularData)
